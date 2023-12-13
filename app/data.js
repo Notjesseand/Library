635dd00ad0e091a7a82e357ed245cb8736469272
Notjessand <jessennorom@gmail.com>
2023-12-13 21:29:43 +0100

    initial commit

---
+++ app/data.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    text: "Explore the World of Infinite Possibilities with Our Library",
+  },
+  {
+    text: "Discover a Vast Collection of eBooks, Journals, and More",
+  },
+  {
+    text: "Unlimited Access to Knowledge Anytime, Anywhere",
+  },
+  {
+    text: "Transform Your Learning Experience with Our Digital Resources",
+  },
+  {
+    text: "Check Out Our Latest eBook Additions!",
+  },
+  {
+    text: "Connect with Like-minded Readers: Join Our Online Community.",
+  },
+  {
+    text: "Start Your Journey Today: Sign Up for Free Access",
+  },
+];
---
+++ components/carousel.tsx
@@ -0,0 +1,49 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+// import { Pagination, Navigation } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import {data} from '../app/data'
+
+const Carousel = () => {
+  return (
+    <Swiper
+      spaceBetween={30}
+      // slidesPerView={3}
+       breakpoints={{
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 1,
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 3,
+        },}}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      // pagination={{ type: "progressbar" }}
+      modules={[Autoplay, Pagination]}
+      className="mySwiper hidden md:block "
+    >
+      {data.map((item, index)=>(
+
+      <SwiperSlide key={index} className="cursor-pointer flex bg-inherit text-white text-center w-full backdrop-blur-3xl rounded-sm    ">
+        <p className="py-6">{item.text}</p>
+      </SwiperSlide>
+      ))}
+      
+    </Swiper>
+  );
+};
+
+export default Carousel;
