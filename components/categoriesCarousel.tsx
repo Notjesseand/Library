3b7005205d393396e1ee2d8127e324150321cce8
Notjessand <jessennorom@gmail.com>
2023-12-18 08:41:58 +0100

    additional updates

--- components/categoriesCarousel.tsx
+++ components/categoriesCarousel.tsx
@@ -36,18 +36,18 @@ const categoriesCarousel = () => {
       }}
       // pagination={{ type: "progressbar" }}
       modules={[Autoplay, Pagination]}
-      className="mySwiper  font-open px-14 sm:px-24"
+      className="mySwiper font-open px-8 sm:px-24 rounded-md"
     >
       {categories.map((item, index) => (
         <SwiperSlide
           key={index}
-          className=" px-24 mt-6 sm:mt-12 h-[40px] inline-block cursor-pointer  text-white text-center w-full  rounded-sm    "
+          className="sm:px-12 px-24 mt-6 sm:mt-12 h-[40px] inline-block cursor-pointer  text-white text-center w-full rounded-md    "
           style={{ backgroundImage: "/drama.jpg" }}
         >
-          <div className="relative group h-44 sm:h-56 bg-gray-500 bg-cover w-44 sm:w-5/6 bg-center mx-auto rounded overflow-hidden transition-opacity duration-300">
+          <div className="relative group h-44 sm:h-56 bg-gray-500 bg-cover w-48 sm:w-5/6 bg-center mx-auto rounded-md overflow-hidden transition-opacity duration-300">
             {/* <div className="absolute inset-0 bg-gradient-to-t from-[#131414] to-black h-full m-0 p-0 opacity-60"></div> */}
-            <div className="absolute inset-0 z-20 flex items-center justify-center bg-gradient-to-b from-[#363636] to-black hover:opacity-60 opacity-70">
-              <p className="mt-3 capitalize text-xl sm:text-2xl font-open">
+            <div className="absolute rounded-md inset-0 z-20 flex items-center justify-center bg-gradient-to-b from-[#363636] to-black hover:opacity-60 opacity-70">
+              <p className="mt-3 capitalize text-xl sm:text-2xl font-open px-1">
                 {item.category}
               </p>
             </div>
