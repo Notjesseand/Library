f3fafc686a97246a1c46d775955731446b4b6081
Notjessand <jessennorom@gmail.com>
2023-12-18 08:14:57 +0100

    additional updates

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React from "react";
 import { IoBookSharp } from "react-icons/io5";
 import { LuLibrary } from "react-icons/lu";
 import { IoPerson } from "react-icons/io5";
 import { FaStar } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 
-
 export default function footer() {
   return (
-    <div className="fixed bottom-0 h-14 w-full md:hidden flex z-20 px-5 justify-between">
-      <FaSearch className=" text-3xl font-bold" />
-      <IoBookSharp className=" text-3xl" />
-      <FaStar className=" text-3xl" />
-      <LuLibrary className=" text-3xl" />
-      <IoPerson className=" text-3xl" />
+    <div className="fixed bottom-0 h-16    w-full md:hidden flex justify-between items-center z-20 px-5 font-open bg-blur backdrop-blur-3xl   ">
+      <div className="flex flex-col items-center">
+        <FaSearch className="text-3xl font-bold" />
+        <p className="text-sm text-gray-400 font-custom">Browse</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <IoBookSharp className="text-3xl" />
+        <p className="text-sm text-gray-400 font-custom">Books</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <FaStar className="text-3xl" />
+        <p className="text-sm text-gray-400 font-custom">Favorites</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <LuLibrary className="text-3xl" />
+        <p className="text-sm text-gray-400 font-custom">Library</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <IoPerson className="text-3xl" />
+        <p className="text-sm text-gray-400 font-custom">Profile</p>
+      </div>
     </div>
   );
 }
