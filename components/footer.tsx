e5b46665d3ec70fd7e7a4e613a8a171decad72d0
Notjessand <jessennorom@gmail.com>
2023-12-23 13:31:00 +0100

    additional updates

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,33 +11,33 @@ export default function footer() {
     <div className="fixed bottom-0 h-16 w-full md:hidden z-20 px-2 font-open backdrop-blur-3xl     pt-2 grid grid-cols-5">
       <div className="flex flex-col items-center">
         <FaSearch className="text-3xl font-bold" />
-        <p className="text-sm text-gray-400 font-custom">Browse</p>
+        <p className="text-[12px] text-gray-400 font-custom">Browse</p>
       </div>
         <Link href="/categories">
       <div className="flex flex-col items-center">
           <IoBookSharp className="text-3xl" />
-          <p className="text-sm text-gray-400 font-custom">Categories</p>
+          <p className="text-[12px] text-gray-400 font-custom">Categories</p>
       </div>
         </Link>
         <Link href="/favourites">
       <div className="flex flex-col items-center">
           <FaStar className="text-3xl" />
-          <p className="text-sm text-gray-400 font-custom">Faves</p>
+          <p className="text-[12px] text-gray-400 font-custom">Faves</p>
       </div>
         </Link>
       {/* <div className="flex flex-col items-center">
         <Link href="/favourites">
           <FaStar className="text-3xl" />
-          <p className="text-sm text-gray-400 font-custom">Favorites</p>
+          <p className="text-[12px] text-gray-400 font-custom">Favorites</p>
         </Link>
       </div> */}
       <div className="flex flex-col items-center">
         <LuLibrary className="text-3xl" />
-        <p className="text-sm text-gray-400 font-custom">Library</p>
+        <p className="text-[12px] text-gray-400 font-custom">Library</p>
       </div>
       <div className="flex flex-col items-center">
         <IoPerson className="text-3xl" />
-        <p className="text-sm text-gray-400 font-custom">Profile</p>
+        <p className="text-[12px] text-gray-400 font-custom">Profile</p>
       </div>
     </div>
   );
