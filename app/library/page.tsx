4c8ab4b5930138da3a1cf24576697f20dcfb18da
Notjessand <jessennorom@gmail.com>
2024-01-14 19:05:21 +0100

    additonal updates

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
         </div>
         <Searchbar />
       </div>
-      <section className=" px-4 md:px-10 pt-8">
+      <section className=" px-4 md:px-10 pt-4 sm:pt-8">
         <p className="text-2xl md:text-3xl font-bold">My Library</p>
         {/* books */}
         <div className="grid md:grid-cols-3 sm:grid-cols-2 pt-6 gap-x-5 gap-y-6">
